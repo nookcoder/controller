d773f91f90b45e73ed6fea95a9c74d0186a2ce7a
wook <gus5427@naver.com>
2023-02-17 20:10:57 +0900

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MockModule } from './mock/mock.module';
 
 @Module({
-  imports: [],
+  imports: [MockModule],
   controllers: [AppController],
   providers: [AppService],
 })
